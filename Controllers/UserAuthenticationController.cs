f76b3f92f863ef5f8f50257c32f702b6a74d054c
FrancesscoMoroni <63257919+FrancesscoMoroni@users.noreply.github.com>
2023-09-16 13:58:44 +0200

    Implementacja logowania (początkowy kod)

---
+++ Controllers/UserAuthenticationController.cs
@@ -0,0 +1,40 @@
+﻿using LifeWithFood.Data;
+using LifeWithFood.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LifeWithFood.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserAuthenticationController : ControllerBase
+    {
+
+        private readonly LifeWithFoodDbContext _dbcontext;
+
+
+        public UserAuthenticationController(LifeWithFoodDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpPost]
+        [Route("userlogin")]
+        public async Task<ActionResult<bool>> Login(UserDto userDto)
+        {
+            return Ok(Autherization(userDto));
+        }
+
+        private bool Autherization(UserDto userDto)
+        {
+            try
+            {
+                return !_dbcontext.Users.Where(e => e.Login == userDto.login && e.Password == userDto.password).IsNullOrEmpty();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ DTO/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LifeWithFood.DTO
+{
+    public class UserDto
+    {
+        public string login { get; set; } = String.Empty;
+        public string password { get; set; } = String.Empty;
+    }
+}
